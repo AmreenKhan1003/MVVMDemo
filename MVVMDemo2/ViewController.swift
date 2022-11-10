53acb14b3f5b4248b10d9ae46880167a0ddb055e
AmreenKhan1003 <amreen10032000@gmail.com>
2022-11-09 21:02:49 -0800

    all done

--- MVVMDemo2/ViewController.swift
+++ MVVMDemo2/ViewController.swift
@@ -27,25 +27,6 @@ class ViewController: UIViewController {
             //print(arrArticles)
         }
         
-//        var viewmodelobj = NewsDatas()
-//
-//        viewmodelobj.getDataFromAPI { [self] news, error in
-//            var num = news?.articles?.count
-//            for i in 0...num!-1{
-//                let d = news?.articles![i].description
-//                //print(d)
-//                //userCompletionHandler(arr.append(d!), error)
-//                //print(arrArticles[i])
-//                if d != nil{
-//                    arrArticles.append(d!)}
-//
-//            }
-//            print(arrArticles)
-//        }
-        //view -> viewModel
-        
-        //print(arrArticles)
-        //arrArticles = [ "Amreen Khan", "Safwan Modak", "Amreen Khan", "Safwan Modak", "Amreen Khan", "Safwan Modak", "Amreen Khan", "Safwan Modak", "Amreen Khan", "Safwan Modak"]
         
     }
     func getdata(userCompletionHandler: @escaping (Array<String>?, Error?) -> Void) {
